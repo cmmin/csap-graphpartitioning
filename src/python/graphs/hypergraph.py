9f5939764d4a2884f7eab6bdda2e8e53e23e21b3
cmmin <carlo992@gmail.com>
2017-01-22 16:17:14 +0000

    Created prototype hypergraph to graph code

---
+++ src/python/graphs/hypergraph.py
@@ -0,0 +1,54 @@
+
+# http://nbviewer.jupyter.org/github/atteroTheGreatest/hypergraph/blob/master/notebooks/hypergraphs_1.ipynb?create=1
+
+# clique: https://en.wikipedia.org/wiki/Clique_(graph_theory)
+
+
+
+
+
+def hyperToGraph():
+    xpins = [0, 5, 7, 11, 13, 15, 19, 21, 25, 27, 29, 31]
+    pins = [2, 3, 5, 6, 9, 0, 1, 0, 1, 2, 3, 1, 3, 4, 5, 4, 5, 6, 7, 6, 7, 8, 9, 10, 11, 8, 11, 8, 10, 2, 5]
+
+    print(len(xpins))
+    print(len(pins))
+
+    vertices = {}
+
+    for xpinID in range(0, len(xpins)):
+        pinIDStart = xpins[xpinID]
+
+        if(xpinID + 1 == len(xpins)):
+            # last
+            break
+
+        pinIDEnd = xpins[xpinID + 1]
+
+        hyperedges = []
+        for pinID in range(pinIDStart, pinIDEnd):
+            #s += str(pins[pinID]) + ','
+            hyperedges.append(pins[pinID])
+
+        while len(hyperedges) > 0:
+            # take the last element
+            vertex = hyperedges[len(hyperedges) - 1]
+            hyperedges.pop()
+
+            for other in hyperedges:
+                addEdge(vertices, vertex, other)
+    for vertex in vertices:
+        print("Vertex", vertex, "edges:", vertices[vertex])
+
+
+
+def addEdge(vertices, v1, v2):
+    if v1 not in vertices:
+        vertices[v1] = []
+    if v2 not in vertices:
+        vertices[v2] = []
+
+    if v2 not in vertices[v1]:
+        vertices[v1].append(v2)
+    if v1 not in vertices[v2]:
+        vertices[v2].append(v1)
--- src/python/playground.py
+++ src/python/playground.py
@@ -1,5 +1,6 @@
 import scotch.graph_mapper as gm
 
+import graphs.hypergraph as hg
 
 if __name__ == '__main__':
     s = "101"
@@ -7,5 +8,6 @@ if __name__ == '__main__':
     metisPath = '../../data/oneshot_fennel_weights.txt'
     scotchPath = "../../tools/scotch/lib/macOS/libscotch.dylib"
 
-    mapper = gm.partitionMetis(scotchPath, metisPath)
-    
+    #mapper = gm.partitionMetis(scotchPath, metisPath)
+
+    hg.hyperToGraph()
