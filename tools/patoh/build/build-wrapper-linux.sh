93e3b53b274879edcb32eabf63a0f046bf1ca541
cmmin <carlo992@gmail.com>
2017-02-19 16:35:05 +0100

    Added wrapper in c++ for PaToH so that it can be rebuilt as a shared library

---
+++ tools/patoh/build/build-wrapper-linux.sh
@@ -0,0 +1,1 @@
+g++ -fPIC -shared -Llinux/ -lpatoh -lm patoh_wrapper.cpp -o ../lib/linux/libpatoh.so
---
+++ tools/patoh/build/build-wrapper-macos.sh
@@ -0,0 +1,1 @@
+g++ -fPIC -shared -LmacOS/ -lpatoh -lm patoh_wrapper.cpp -o ../lib/macOS/libpatoh.dylib
